0cd52de53c6e507d1b26337d812f8d36c8097a0b
tygreenwoodOSU <greenwot@oregonstate.edu>
2024-04-23 11:26:17 -0700

    Adds endpoints to server.js

--- code/node_server/server.js
+++ code/node_server/server.js
@@ -3,7 +3,40 @@ const app = express();
 
 const port = process.env.PORT || 8000;
 
+let alertType = "Safe";
+let drill = false;
+let customAlert = "";
+
+const makeMessage = () => {
+    return alertType.replace(/([A-Z])/g, ' $1').trim() + (drill ? " Drill" : "") + (customAlert ? " " + customAlert : "");
+}
+
+const validAlert = (s) => (s === "Safe" || s === "ShelterInPlace" || s === "Exit");
+
 app.use(express.json());
+
+app.get('/', (req, res, next) => {
+    res.status(200).send(makeMessage());
+});
+
+app.put('/', (req, res, next) => {
+   if (req.body.alertType && validAlert(req.body.alertType)) {
+       alertType = req.body.alertType;
+       drill = !!req.body.drill;
+       res.status(200).send(makeMessage());
+   } else {
+       res.status(400).send();
+   }
+});
+
+app.put('/custom', (req, res, next) => {
+    if (req.body.message) {
+        customAlert = req.body.message;
+        res.status(200).send(makeMessage());
+    } else {
+        res.status(400).send();
+    }
+});
 
 app.listen(port, function () {
     console.log("== Server is listening on port:", port)
